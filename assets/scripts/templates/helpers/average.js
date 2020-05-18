38629284d72e593bca36fbcd57dcc7d8f60cd347
ztosyl <ztsapos@gmail.com>
2020-05-18 15:54:30 -0400

    Ratings display
    Added 'Ratings' section that displays average rating, rounded.
    Added 'average' helper to do caclulation in handlebars.
    Added 'None.' for recipes with no ratings.

---
+++ assets/scripts/templates/helpers/average.js
@@ -0,0 +1,22 @@
+// This helper would be used in a .handlebars file
+// with the syntax {{limit title 20}}
+// calculates average of all the numbers in an array
+
+'use strict'
+
+const average = (numArray) => {
+if (!numArray) {
+    return 'None.'
+  } else if (numArray.length === 0) {
+    return 'None.'
+  } else {
+    let sum = 0
+    for (let i = 0; i < numArray.length; i++) {
+      sum += numArray[i]
+    }
+    const avg = (sum / numArray.length)
+    return Math.round(avg)
+  }
+}
+
+module.exports = average
